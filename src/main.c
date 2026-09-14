6ad4c46dc5276a68c662a49241b92b9ad552955a
unknown <caldelan@outlook.com>
2026-09-14 13:09:26 +0200

    testing temp sensor readout on display

--- src/main.c
+++ src/main.c
@@ -8,11 +8,16 @@
 #include "wifi_manager.h"
 #include "mqtt_manager.h"
 
+#include "LCD.h"
+
 static const char *TAG = "MAIN";
 #define WATER_TEMP_GPIO 4
 
 void app_main(void)
 {
+    //Initialise the LCD display
+    LCD_init();
+
     // Vänta lite så Serial Monitor hinner ansluta
     vTaskDelay(pdMS_TO_TICKS(3000));
 
@@ -52,12 +57,27 @@ void app_main(void)
 
     //Sensor Vattentempratur =(adress 8F0B2576714BFC28)
     //Sensor Lufttemperatur =(adress 780B25764C430E28)
+
+    
+
     while (1) {
         float temps[2];
+        float temporary1 = 15.66;
+        float temporary2 = 65.5;
+
+        char lcd_temp[32];
+        char lcd_hum[32];
+
         if (ds18b20_sensor_read_all(temps, 2) == ESP_OK) {
             ESP_LOGI(TAG, "Sensor 0 = Vattentempratur: %.2f C", temps[0]);
             ESP_LOGI(TAG, "Sensor 1 = Lufttemperatur: %.2f C", temps[1]);
         }
+
+        snprintf(lcd_temp, sizeof(lcd_temp), "T:%.1f IN:%.1f", temps[1], temporary1);
+        snprintf(lcd_hum, sizeof(lcd_hum), "H:%.1f W:%.1f", temporary2, temps[0]);
+
+        LCD_print_at(0,0,lcd_temp);
+        LCD_print_at(0,1,lcd_hum);
 
         mqtt_publish_test();
 
