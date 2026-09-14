64c8cca40759d1e2fc0016c5d2102d9e68d35cb7
PatDahl22 <pattaravarat.dahl@gmail.com>
2026-09-14 11:32:38 +0200

    security: remove Wi-Fi credentials from example config

--- src/secrets.example.h
+++ src/secrets.example.h
@@ -1,7 +1,7 @@
 #ifndef SECRETS_EXAMPLE_H
 #define SECRETS_EXAMPLE_H
 
-#define WIFI_SSID "WIFIHUB_27028A"
-#define WIFI_PASS "qwp352wx"
+#define WIFI_SSID "DITT_WIFI_NAME"
+#define WIFI_PASS "DITT_WIFI_PASSWORD"
 
 #endif
